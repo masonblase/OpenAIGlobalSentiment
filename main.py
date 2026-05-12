4e2305b13b666dec9619034ff49cb90686290297
masonblase <dlf514@york.ac.uk>
2026-05-12 18:42:23 +0100

    Normalized and removed urlToImage column

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import pandas as pd
 import requests
 from newsapi import NewsApiClient
 
+# Extract data from NewsAPI
 newsapi = NewsApiClient(api_key = NEWSAPI_KEY"")
-
 all_articles = newsapi.get_everything(q = "OpenAI")
 
-print(all_articles)
-
+# Normalize data and put into DataFrame
+df = pd.json_normalize(all_articles["articles"])
+df = df.drop("urlToImage", axis = 1)
+print(df.columns.tolist())

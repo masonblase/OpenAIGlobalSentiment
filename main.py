140e9b96a936849c263cd0e03be7b873e306953b
masonblase <dlf514@york.ac.uk>
2026-05-12 19:20:08 +0100

    Translate content in articles to English

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+# -- Prerequisites --
+# pip install deep_translator
+# pip install newsapi-python
+# pip install pandas
+# pip install requests
+
+from deep_translator import (GoogleTranslator)
+from newsapi import NewsApiClient
 import pandas as pd
 import requests
-from newsapi import NewsApiClient
 
 # Extract data from NewsAPI
 newsapi = NewsApiClient(api_key = NEWSAPI_KEY"")
 all_articles = newsapi.get_everything(q = "OpenAI")
 
-# Normalize data and put into DataFrame
+# Normalize data and clean data
 df = pd.json_normalize(all_articles["articles"])
 df = df.drop("urlToImage", axis = 1)
-print(df.columns.tolist())
+
+# Translate content
+df["content"] = df["content"].str[:5000]
+df["content"] = GoogleTranslator(source = "auto", target = "en").translate(text = df["content"]) 
+print(df["content"])

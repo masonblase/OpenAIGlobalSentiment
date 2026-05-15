420eed270b6c7fe335eb5d6d6ec831ef2cae7d9a
masonblase <dlf514@york.ac.uk>
2026-05-15 15:34:06 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # OpenAIGlobalSentiment
-Program that obtains sentiment data about OpenAI from news articles written in a variety of languages. Select desired source language from dropdown menu, then click "Write csv". 
+I was curious about how public opinion around OpenAI has changed over time, given the hot discourse that currently surrounds artificial intelligence.
+
+To answer my questions, I wrote this program that extracts sentiment data about OpenAI from news articles written in a variety of languages from a plethora of places. The data are then written onto a csv file for further analysis/visualization in your preferred software.
+
+## How to use:
+Select desired source language from dropdown menu, then click "Write csv".
--- ui.py
+++ ui.py
@@ -1,10 +1,12 @@
 import customtkinter as ctk
 
+# -- UI CODE --
 def openaiglobalsentiment_ui(app, get_newsapi_data):
     app.title("OpenAI Global Sentiment")
     app.geometry("400x150")
     app.grid_columnconfigure(0, weight = 1)
 
+    # Supported languages
     languages = {
     "Arabic": "ar",
     "German": "de",
@@ -20,8 +22,10 @@ def openaiglobalsentiment_ui(app, get_newsapi_data):
     "Chinese": "zh",
     }
 
+    # Dropdown menu
     language_combobox = ctk.CTkComboBox(app, values = list(languages.keys()))
     language_combobox.grid(row = 0, column = 1, padx = 20, pady = 20, sticky = "w")
 
+    # Write csv button
     button = ctk.CTkButton(app, text = "Write csv", command = lambda: get_newsapi_data(languages, language_combobox))
     button.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = "ew")
